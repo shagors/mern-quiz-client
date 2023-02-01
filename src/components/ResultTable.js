1897f8c863e65815ec836019071843912f581afb
shajjad Shagor <shajjadshagor@gmail.com>
2023-01-31 23:22:53 -0800

    result get from db and results show in table display

--- src/components/ResultTable.js
+++ src/components/ResultTable.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getServerData } from '../helper/helper';
 
 const ResultTable = () => {
+
+    const [data, setData] = useState([]);
+    useEffect( () => {
+        getServerData(`${process.env.REACT_APP_SERVER_HOSTNAME}/api/result`, (res) => {
+            setData(res);
+        });
+    }, []);
+
   return (
     <div>
         <table>
@@ -13,12 +22,17 @@ const ResultTable = () => {
                 </tr>
             </thead>
             <tbody>
-                <tr className='table-body'>
-                    <td>Shajjad</td>
-                    <td>03</td>
-                    <td>20</td>
-                    <td>Passed</td>
-                </tr>
+                { !data ?? <div>No Data Found</div> }
+                {
+                    data?.map((v, i) => (
+                        <tr className='table-body' key={i}>
+                            <td>{v?.userName || ''}</td>
+                            <td>{v?.attempts || 0}</td>
+                            <td>{v?.points || 0}</td>
+                            <td>{v?.achived || ''}</td>
+                        </tr>
+                    ))
+                }
             </tbody>
         </table>
     </div>
